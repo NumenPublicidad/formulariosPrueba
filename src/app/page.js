aa1b5ab61db44ca8a081a70b5969e58642b21d75
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-12-19 19:09:20 -0300

    Add 3 forms 1,2,and 3

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+
+
+export default function Home() {
+  return (
+      <main className=" border mt-28 h-screen">
+        <div className="text-white">
+          <p>PREPARATE</p>
+          <p>PARA EL </p>
+          <div>
+            <p>P</p>
+            <p>r</p>
+            <p>e</p>
+            <p>s</p>
+            <p>e</p>
+            <p>n</p>
+            <p>t</p>
+            <p>e</p>
+          </div>
+
+        </div>
+      </main>
+  );
+}
